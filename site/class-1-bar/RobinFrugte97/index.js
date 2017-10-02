2984118588a127944f5c5f24419eab436af4b3f5
RobinFrugte97 <robin.frugte@hotmail.com>
2017-10-02 15:49:10 +0200

    Update index.js

--- site/class-1-bar/RobinFrugte97/index.js
+++ site/class-1-bar/RobinFrugte97/index.js
@@ -1,3 +1,5 @@
+// Original code by Mike Bostock - https://bl.ocks.org/mbostock/3885304
+
 var svg = d3.select("svg"),
     margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = +svg.attr("width") - margin.left - margin.right,
