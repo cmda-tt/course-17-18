7c3a63098d5b4822377f6f0c5189bafeae0561fd
Kangyun Wang <wky@192.168.2.11>
2017-10-03 21:12:36 +0200

    Added a simple eslint. Cleaned up the html, css, js files. Refactored index.js

--- site/class-2-style/kyunwang/index.js
+++ site/class-2-style/kyunwang/index.js
@@ -1,1 +1,77 @@
-function render(b,c){if(b)throw b;var a=svg.selectAll('.arc').data(pie(c)).enter().append('g').attr('class','arc');a.append('path').attr('d',arc).style('fill',fill),a.append('text').attr('transform',transform).attr('dy','.35em').text(age)}function type(a){return a.population=Number(population(a)),a}function transform(a){return'translate('+arc.centroid(a)+')'}function fill(a){return color(age(a))}function age(a){return a.data.age}function population(a){return a.population}var width=500,height=500,radius=Math.min(width,height)/2,colors=['hsl(45, 99%, 59%)','hsl(96, 99%, 59%)','hsl(148, 99%, 59%)','hsl(199, 99%, 59%)','hsl(250, 99%, 59%)','hsl(302, 99%, 59%)','hsl(353, 99%, 59%)'],color=d3.scaleOrdinal().range(colors),arc=d3.arc().outerRadius(radius-16).innerRadius(radius-128),pie=d3.pie().sort(null).value(population),svg=d3.select('body').append('svg').attr('width',width).attr('height',height).attr('viewBox',[0,0,width,height].join(' ')).append('g').attr('transform','translate('+width/2+','+height/2+')');d3.csv('index.csv',type,render)
+function renderChart(err, data) {
+	if (err) throw err;
+	var chart = createSvg
+		.selectAll('.arc')
+		.data(initiatePie(data))
+		.enter()
+		.append('g')
+		.attr('class', 'arc');
+
+	chart
+		.append('path')
+		.attr('d', arcSize)
+		.style('fill', getFill);
+		
+	chart
+		.append('text')
+		.attr('transform', getTransform)
+		.attr('dy', '.35em')
+		.text(getAge);
+}
+
+function modifyRow(data) {
+	return (data.population = Number(getPopulation(data))), data;
+}
+
+function getTransform(a) {
+	return 'translate(' + arcSize.centroid(a) + ')';
+}
+
+function getFill(a) {
+	return colorScale(getAge(a));
+}
+
+function getAge(a) {
+	return a.data.age;
+}
+
+function getPopulation(a) {
+	return a.population;
+}
+
+var width = 500,
+	height = 500,
+	radius = Math.min(width, height) / 2,
+	colors = [
+		'hsl(45, 99%, 59%)',
+		'hsl(96, 99%, 59%)',
+		'hsl(148, 99%, 59%)',
+		'hsl(199, 99%, 59%)',
+		'hsl(250, 99%, 59%)',
+		'hsl(302, 99%, 59%)',
+		'hsl(353, 99%, 59%)'
+	];
+
+var colorScale = d3.scaleOrdinal().range(colors);
+
+var arcSize = d3
+	.arc()
+	.outerRadius(radius - 16)
+	.innerRadius(radius - 128);
+
+var initiatePie = d3
+	.pie()
+	.sort(null)
+	.value(getPopulation);
+
+// Create and append the svg
+var createSvg = d3
+	.select('body')
+	.append('svg')
+	.attr('width', width)
+	.attr('height', height)
+	.attr('viewBox', [0, 0, width, height].join(' '))
+	.append('g')
+	.attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+
+d3.csv('index.csv', modifyRow, renderChart);
