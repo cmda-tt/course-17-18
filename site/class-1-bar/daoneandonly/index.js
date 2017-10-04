ac87239c8aebfe8e177aefeed674e0f0f0217b84
daoneandonly <stevenawhitton@gmail.com>
2017-10-04 14:55:51 +0200

    Add files @daoneandonly

---
+++ site/class-1-bar/daoneandonly/index.js
@@ -0,0 +1,103 @@
+//start with variables
+
+//SVG starts here
+var svg = d3.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 140},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var x0 = d3.scaleBand()
+    .rangeRound([0, width])
+    .paddingInner(0.1);
+
+//padding in between bars
+var x1 = d3.scaleBand()
+    .padding(0);
+
+//defining the scale of the graph
+var y = d3.scaleLinear()
+    .rangeRound([height, 0]);
+
+//Array color codes, edited with Adobe Kuler
+var z = d3.scaleOrdinal()
+    .range(["#3C3E85", "#2B686F", "#01A2A6", "#78D18C", "#BDF25D", "#004BA2", "#053F6F"]);
+
+//loading in CSV through ds3
+d3.csv("data.csv", function(d, i, columns) {
+  for (var i = 1, n = columns.length; i < n; ++i) d[columns[i]] = +d[columns[i]];
+  return d;
+}, function(error, data) {
+  if (error) throw error;
+
+  var keys = data.columns.slice(1);
+
+//replaced 'State' with 'Day'
+//this is where the range of the graph gets made
+  x0.domain(data.map(function(d) { return d.Day; }));
+  x1.domain(keys).rangeRound([0, x0.bandwidth()]);
+  y.domain([0, d3.max(data, function(d) { return d3.max(keys, function(key) { return d[key]; }); })]).nice();
+
+
+//creating bars with data
+  g.append("g")
+    .selectAll("g")
+    .data(data)
+    .enter().append("g")
+      .attr("transform", function(d) { return "translate(" + x0(d.Day) + ",0)"; })
+    .selectAll("rect")
+    .data(function(d) { return keys.map(function(key) { return {key: key, value: d[key]}; }); })
+    .enter().append("rect")
+      .attr("x", function(d) { return x1(d.key); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("width", x1.bandwidth())
+      .attr("height", function(d) { return height - y(d.value); })
+      .attr("fill", function(d) { return z(d.key); });
+
+//creating axis
+  g.append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x0));
+
+//text on vertical axis
+  g.append("g")
+      .attr("class", "axis")
+      .call(d3.axisLeft(y).ticks(null, "s"))
+    .append("text")
+      .attr("x", 2)
+      .attr("y", y(y.ticks().pop()) + 0.5)
+      .attr("dy", "0.32em")
+      .attr("fill", "#000")
+      .attr("font-weight", "bold")
+      .attr("text-anchor", "start")
+      .text("Minuten media Gebruik");
+
+
+
+//legenda SVG
+
+//legenda text
+  var legend = g.append("g")
+      .attr("font-family", "Open Sans")
+      .attr("font-size", 10)
+      .attr("text-anchor", "end")
+    .selectAll("g")
+    .data(keys.slice().reverse())
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+//legenda rectangles
+
+  legend.append("rect")
+      .attr("x", width - 19)
+      .attr("width", 19)
+      .attr("height", 19)
+      .attr("fill", z);
+
+  legend.append("text")
+      .attr("x", width - 24)
+      .attr("y", 9.5)
+      .attr("dy", "0.32em")
+      .text(function(d) { return d; });
+});
