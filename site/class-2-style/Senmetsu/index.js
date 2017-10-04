3fde608e4ef065c52bee1f31dbcbbff072abb9a1
Titus Wormer <tituswormer@gmail.com>
2017-10-04 09:06:18 +0200

    site:class-2-style:Senmetsu: fix

--- site/class-2-style/Senmetsu/index.js
+++ site/class-2-style/Senmetsu/index.js
@@ -22,7 +22,7 @@ var width = 500,
 
 d3.csv('index.csv', type, render)
 
-function chartCreation(b, c) {
+function render(b, c) {
     if (b) throw b;
     var a = svg.selectAll('.arc')
     .data(pie(c))
