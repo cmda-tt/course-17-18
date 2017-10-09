996f718c966640de0ddf4dd6705e8550412e7ebf
DesleyAalderink <desley-aalderink@hotmail.com>
2017-10-09 13:04:15 +0200

    @DesleyAalderink

--- site/class-3-transition/DesleyAalderink/index.js
+++ site/class-3-transition/DesleyAalderink/index.js
@@ -32,70 +32,70 @@ d3.csv("flare.csv", function(d) {
       }
     });
 
-  var node = svg.selectAll(".node")
-    .data(pack(root).leaves())
-    .enter().append("g")
-    .attr("class", "node")
-    .attr("transform", function(d) {
-      return "translate(" + d.x + "," + d.y + ")";
-    });
+var node = svg.selectAll(".node")
+  .data(pack(root).leaves())
+  .enter().append("g")
+  .attr("class", "node")
+  .attr("transform", function(d) {
+    return "translate(" + d.x + "," + d.y + ")";
+  });
 
-  node.append("circle")
-    .attr("id", function(d) {
-      return d.id;
-    })
-    .attr("r", function(d) {
-      return d.r;
-    })
-    .style("fill", function(d) {
-      return color(d.package);
-    });
+node.append("circle")
+  .attr("id", function(d) {
+    return d.id;
+  })
+  .attr("r", function(d) {
+    return d.r;
+  })
+  .style("fill", function(d) {
+    return color(d.package);
+  });
 
-  node.append("clipPath")
-    .attr("id", function(d) {
-      return "clip-" + d.id;
-    })
-    .append("use")
-    .attr("xlink:href", function(d) {
-      return "#" + d.id;
-    });
+node.append("clipPath")
+  .attr("id", function(d) {
+    return "clip-" + d.id;
+  })
+  .append("use")
+  .attr("xlink:href", function(d) {
+    return "#" + d.id;
+  });
 
-  node.append("text")
-    .attr("clip-path", function(d) {
-      return "url(#clip-" + d.id + ")";
-    })
-    .selectAll("tspan")
-    .data(function(d) {
-      return d.class.split(/(?=[A-Z][^A-Z])/g);
-    })
-    .enter().append("tspan")
-    .attr("x", 0)
-    .attr("y", function(d, i, nodes) {
-      return 13 + (i - nodes.length / 2 - 0.5) * 10;
-    })
-    .text(function(d) {
-      return d;
-    });
+node.append("text")
+  .attr("clip-path", function(d) {
+    return "url(#clip-" + d.id + ")";
+  })
+  .selectAll("tspan")
+  .data(function(d) {
+    return d.class.split(/(?=[A-Z][^A-Z])/g);
+  })
+  .enter().append("tspan")
+  .attr("x", 0)
+  .attr("y", function(d, i, nodes) {
+    return 13 + (i - nodes.length / 2 - 0.5) * 10;
+  })
+  .text(function(d) {
+    return d;
+  });
 
-  node.append("title")
-    .text(function(d) {
-      return d.id + "\n" + format(d.value);
-    });
+node.append("title")
+  .text(function(d) {
+    return d.id + "\n" + format(d.value);
+  });
 
-  // Circles fill with mouse over
-  d3.selectAll('circle')
-    .on('mouseover', function(d){
-    d3.select(this)
-    .transition()
-    .duration(950)
-    .ease(d3.easeBounceIn)
-    .style("fill", "blue");
+// Circles fill with mouse over
+d3.selectAll('circle')
+  .on('mouseover', function(d){
+  d3.select(this)
+  .transition()
+  .duration(950)
+  .ease(d3.easeBounceIn)
+  .style("fill", "blue");
 })
 
-  // Delete circle
-  d3.selectAll('circle')
-    .on('click', function(d) {
-    d3.select(this)
-    this.remove()
-    })
+// Delete circle
+d3.selectAll('circle')
+  .on('click', function(d) {
+  d3.select(this)
+  this.remove()
+  })
 });
