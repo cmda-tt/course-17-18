d91693a7c9ffe5275ddf97314a84ee1b6d8caf09
Luisa Braga <32274338+mymphy@users.noreply.github.com>
2017-10-13 09:26:21 +0200

    Add @mymphy
    Closes GH-522.

---
+++ site/class-4-interaction/mymphy/index.js
@@ -0,0 +1,168 @@
+//Based on https://bl.ocks.org/d3noob/bdf28027e0ce70bd132edc64f1dd7ea4 by Mike Bostock
+
+// set the dimensions and margins of the graph
+var margin = { top: 20, right: 20, bottom: 30, left: 60};
+var width = 960 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
+
+// Set ranges of the graph
+var x = d3.scaleBand()
+  .range([0, width])
+  .padding(0.2);
+var y = d3.scaleLinear()
+  .range([height, 0]);
+
+// create svg inside the body
+var svg = d3.select("body").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform",
+  "translate(" + margin.left + "," + margin.top + ")");
+
+// get the data
+d3.tsv("index.tsv", function(error, data) {
+  if (error) throw error;
+
+// load the data
+data.forEach(function(d) {
+  d.frequency = +d.frequency;
+});
+
+// domains gives the range of the graph 
+x.domain(data.map(function(d) {
+  return d.letter;
+}));
+y.domain([0, d3.max(data, function(d) {
+  return d.frequency;
+})]);
+
+// Assign color to data
+var colors = d3.scaleLinear()
+.domain([0, d3.max(data, function(d) { return d.frequency; })])
+.range(["#BCF4EC", "#0A4239"]);
+
+// Creates the bars
+svg.selectAll(".bar")
+  .data(data)
+  .enter().append("rect")
+  .attr("class", "bar")
+  .attr("x", function(d) {
+    return x(d.letter);
+  })
+  .attr("width", x.bandwidth())
+  .attr("y", function(d) {
+    return y(d.frequency);
+  })
+  .attr("height", 0)
+  .transition()
+  .duration(200)
+  .delay(function(d, i) {
+    return i * 50;
+  })
+  .attr("height", function(d) {
+    return height - y(d.frequency);
+  })
+  .attr('fill', function(d){return colors(d.frequency)});
+
+// Add the x Axis
+svg.append("g")
+  .attr('class', 'xAxis')
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x));
+
+// Add the y Axis 
+svg.append("g")
+  .attr('class', 'yAxis')
+  .call(d3.axisLeft(y))
+  .append("text") 
+  .attr("class", "label")
+  .attr("transform", "rotate(-90)") 
+  .attr("y", -50) 
+  .text("%");
+  
+
+// Add a button with on click function
+d3.select('.buttons')
+  .append('button')
+  .on('click', highToLow)
+  .attr('class', 'button')
+  .text('High to Low');
+
+// Add a button with on click function
+d3.select('.buttons')
+  .append('button')
+  .on('click', Alphabetic)
+  .attr('class', 'button')
+  .text('Alphabetic');
+
+// highToLow function
+function highToLow() {
+  var x0 = x.domain(data.sort(sortOnFrequency).map(letter)).copy();
+  var transition = svg.transition();
+
+  // Move the bars
+  transition.selectAll('.bar')
+    .delay(function(d, i){
+    return i * 20;
+  })
+    .attr('x', barX0);
+
+  // Move the labels
+  transition.select('.axisX')
+    .call(d3.axisBottom(x))
+    .selectAll('g')
+    .delay(function(d, i){
+      return i * 20;
+    })
+
+  function barX0(d) {
+    return x0(letter(d));
+  }
+  // Returns sorted data on frequency (high to low)
+  function sortOnFrequency(a, b) {
+    return d3.ascending(frequency(b), frequency(a));
+  }
+}
+
+// Alphabetic funcion
+function Alphabetic() {
+  var x0 = x.domain(data.sort(sortOnLetter).map(letter)).copy();
+  var transition = svg.transition();
+
+  // Move the bars
+  transition.selectAll('.bar')
+    .delay(function(d, i){
+      return i * 20;
+    })
+    .attr('x', barX0);
+
+  // Move the labels
+  transition.select('.axisX')
+    .call(d3.axisBottom(x))
+    .selectAll('g')
+    .delay(function(d, i){
+      return i * 20;
+    })
+
+  function barX0(d) {
+    return x0(letter(d));
+  }
+  // Returns data on alphabetic order
+  function sortOnLetter(a, b) {
+    return d3.descending(letter(b), letter(a));
+  }
+
+}
+
+// Data frequency
+function frequency(d) {
+  return d.frequency;
+}
+
+// Data letter
+function letter(d) {
+  return d.letter;
+}
+
+});
---
+++ site/class-4-interaction/mymphy/readme.md
@@ -0,0 +1,52 @@
+# Sort
+This is simple bar chart visualizing the percentage of the letters from the alphabet. This chart was based on [**bl.ock**](https://bl.ocks.org/d3noob/bdf28027e0ce70bd132edc64f1dd7ea4) by [@mbostock](https://github.com/mbostock).
+
+![Alt text](preview.png)
+
+## Data
+Tab-separated values (TSV) with 27 rows and two columns:
+
+* 'letter' — Each letter in the alphabet
+* 'frequency' — Number describing how frequent the corresponding letter
+occurs in the English language
+
+## Features
+* ['d3-array'](https://github.com/d3/d3-array#api-reference)
+— 'd3.max' and 'd3.ascending'
+— Array statistics and searching
+* ['d3-axis'](https://github.com/d3/d3-axis#api-reference)
+— 'd3.axisBottom' and 'd3.axisLeft'
+— Reference marks for scales
+* ['d3-scale'](https://github.com/d3/d3-scale#api-reference)
+— 'd3.scaleBand' and 'd3.scaleLinear'
+— Position encodings
+* ['d3-selection'](https://github.com/d3/d3-selection#api-reference)
+— 'd3.select'
+— Select elements
+* ['d3-timer'](https://github.com/d3/d3-timer#api-reference)
+— 'd3.timeout'
+
+## changes made
+The sort changes I used [this code](https://github.com/cmda-fe3/course-17-18/tree/master/site/class-1-bar/wooorm#readme) as a reference
+* I made 2 sort functions.
+- One to sort it by the value, so from high to low
+```
+function highToLow() {
+var x0 = x.domain(data.sort(sortOnFrequency).map(letter)).copy();
+var transition = svg.transition();
+```
+- Another to sort on alfabetical order.
+```
+function Alphabetic() {
+var x0 = x.domain(data.sort(sortOnLetter).map(letter)).copy();
+var transition = svg.transition();
+```
+* Added a meaningful color to the graph.
+```
+var colors = d3.scaleLinear()
+.domain([0, d3.max(data, function(d) { return d.frequency; })])
+.range(["#BCF4EC", "#0A4239"]);
+```
+
+## License
+GPL-3.0 © Luisa Braga dos Santos
